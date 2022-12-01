760d92a178358d942419b90fdadced2b1ec9f2a8
julio <JulioCavalcanti@proton.me>
2022-12-01 14:30:16 -0300

    :sparkles: add insert method and improving comments

--- src/linkedList/DoublyLinkedList.php
+++ src/linkedList/DoublyLinkedList.php
@@ -1,12 +1,79 @@
 <?php
 namespace Julio\DataStructure\linkedList;
 
+use Julio\DataStructure\Complementary\DoublyNode;
 use Julio\DataStructure\linkedList\LinkedList;
 
 class DoublyLinkedList extends LinkedList {
-    private $tail = null;
+    private ?DoublyNode $tail = null;
+    private ?DoublyNode $head = null;
+    private int $count = 0;
 
-    public function __construct() {
-        parent::__construct();
+    /** insert any element in specified index */
+    public function insert($element, $index): bool {
+        if (!($index >= 0 && $index <= $this->count)) return false;
+        $node = new DoublyNode($element);
+        
+        // if index is zero
+        if ($index == 0) $this->indexIsZero($node);
+
+        // if index is the tail
+        else if ($index == $this->count) $this->indexIsTail($node);
+        
+        // if index is in any position
+        else $this->indexIsInAnyPosition($node, $index);
+
+        $this->count++;
+        return true;
+    }
+
+    /** If the index is zero put the node in head */
+    private function indexIsZero(DoublyNode $node): void {
+        $current = $this->head;
+
+        if ($this->head == null) {
+            $this->head = $node;
+            $this->tail = $node;
+        } else {
+            $node->next = $this->head;
+            $current->prev = $node;
+            $this->head = $node;
+        }
+    }
+
+    /** If the index is tail put the node in last position */
+    public function indexIsTail(DoublyNode $node): void {
+        $current = $this->tail;
+
+        $current->next = $node;
+        $node->prev = $current;
+        $this->tail = $node;
+    }
+
+    /** If the index is in any position put the node in specified position */
+    public function indexIsInAnyPosition(DoublyNode $node, $index): void {
+        $previous = $this->getElementAt($index-1);
+        /** @var DoublyNode */ 
+        $current = $previous->next;
+
+        $node->next = $current;
+        $previous->next = $node;
+        $current->prev = $node;
+        $node->prev = $previous;
+    }
+
+    /** get the size of doubly linked list */
+    public function size() {
+        return $this->count;
+    }
+
+    /** get head of doubly linked list */
+    public function getHead() {
+        return $this->head;
+    }
+
+    /** get tail of doubly linked list */
+    public function getTail() {
+        return $this->tail;
     }
 }
--- tests/linkedList/DoublyLinkedListTest.php
+++ tests/linkedList/DoublyLinkedListTest.php
@@ -2,6 +2,7 @@
 namespace Julio\DataStructure\test\linkedList;
 
 use Julio\DataStructure\Complementary\DoublyNode;
+use Julio\DataStructure\linkedList\DoublyLinkedList;
 use PHPUnit\Framework\TestCase;
 
 class DoublyLinkedListTest extends TestCase {
@@ -18,5 +19,48 @@ class DoublyLinkedListTest extends TestCase {
         self::assertNull($thirdDoublyNode->next);
         self::assertSame(2, $thirdDoublyNode -> prev -> element);
         self::assertSame(2, $firstDoublyNode -> next -> element);
+    }
+
+    public function testInsertInHead() {
+        $doublyLinkedList = new DoublyLinkedList();
+        $doublyLinkedList->insert(2, 0);
+        $doublyLinkedList->insert(3, 0);
+        
+        /** @var DoublyNode */
+        $head = $doublyLinkedList->getHead();
+        $tail = $doublyLinkedList->getTail();
+        
+        self::assertSame(3, $head->element);
+        self::assertSame(2, $head->next->element);
+        self::assertSame(3, $tail->prev->element);
+    }
+
+    public function testInsertInTail() {
+        $doublyLinkedList = new DoublyLinkedList();
+        $doublyLinkedList->insert(2, 0);
+        $doublyLinkedList->insert(3, 1);
+
+        /** @var DoublyNode */
+        $head = $doublyLinkedList->getHead();
+        $tail = $doublyLinkedList->getTail();
+
+        self::assertSame(3, $head->next->element);
+        self::assertSame(2, $tail->prev->element);
+        self::assertSame(3, $tail->element);        
+    }
+
+    public function testInsertManyItems() {
+        $doublyLinkedList = new DoublyLinkedList();
+        $doublyLinkedList->insert(2, 0);
+        $doublyLinkedList->insert(3, 3);
+        $doublyLinkedList->insert(2, 2);
+        $doublyLinkedList->insert(1, 1);
+        $doublyLinkedList->insert(6, 5);
+
+        $tail = $doublyLinkedList->getTail();
+        
+        self::assertSame(2, $doublyLinkedList->size());
+        self::assertSame(1, $tail->element);
+        self::assertSame(2, $tail->prev->element);
     }
 }
